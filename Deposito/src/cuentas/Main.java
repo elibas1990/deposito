be2be5a909da6c6bd5efb812444b98999f8a786b
usuario <usuario@DESKTOP-V3R2CSV>
2025-03-23 00:44:54 +0100

    commit

--- Deposito/src/cuentas/Main.java
+++ Deposito/src/cuentas/Main.java
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
 package cuentas;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -27,35 +27,3 @@ public class Main {
         }
 	}
 }
-=======
-package cuentas;
-
-public class Main {
-
-    public static void main(String[] args) {
-        operativa_cuenta(2500);
-    }
-
-	private static void operativa_cuenta(float cantidad) {
-		CCuenta cuenta1;
-        double saldoActual;
-
-        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = cuenta1.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
-
-        try {
-            cuenta1.retirar(2300);
-        } catch (Exception e) {
-            System.out.print("Fallo al retirar");
-        }
-        try {
-            System.out.println("Ingreso en cuenta");
-            cuenta1.ingresar(695);
-        } catch (Exception e) {
-            System.out.print("Fallo al ingresar");
-        }
-	}
-}
-
->>>>>>> branch 'master' of https://github.com/elibas1990/deposito
